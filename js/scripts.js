5324ab19ef10a31390186a4c4393cfaea9ca5dd5
Alex Garcia and Daniel Bennett <alex@>
2018-10-25 16:33:23 -0700

    working on master function to distribute to other function

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,70 @@
-function romanIndex (input){
+function master (input){
+debugger;
+  var masterOutput = input.split("")
+
+  for (i = 0; i < masterOutput.length; i++){
+    if (([i] === 1-9) && ([i] === "")){
+    return romanOneToNine();
+  }else if (([i]=== 1-9) && ([i] === 0-9) && ([i] !== "")){
+    return romanTenToFifty();
+  }
+
+
+
+
+  }
+}
+
+function romanOneToNine (input){
 
   var output = [input]
-  var numsToTen = [" ","I","II","III","V"]
+  var numsToTen = [" ","I","II","III","V","X"]
      for (i=0; i< numsToTen.length; i ++){
         if ( [i] === output){
 
         }else if (input === 4){
           return (numsToTen[1] + numsToTen[4])
-
         }else if (input === 5){
           return numsToTen[4]
-        }
-
-        if (input === 6){
+        }else if (input === 6){
           return (numsToTen[4] + numsToTen[1])
+        }else if (input === 7) {
+          return (numsToTen[4] + numsToTen[2])
+        }else if (input === 8) {
+          return (numsToTen[4] + numsToTen[3])
+        }else if (input === 9) {
+          return (numsToTen[1] + numsToTen[5])
         }
-
     }
-
 return numsToTen[output]
 }
 
+function romanTenToFifty(input){
+  romanOneToNine ()
+  debugger;
+  input.toString()
+  input.split()
+  var output = [input]
+  var numsToFifty = [" ","I","II","III","V","X", "L", "C"]
+     for (i=0; i< numsToTen.length; i ++){
+        if ( [i] === output[1]){
+        }else if (output === [1,0]){
+          return (numsToTen[6])
+        }else if (input === 5){
+          return numsToTen[4]
+        }else if (input === 6){
+          return (numsToTen[4] + numsToTen[1])
+        }else if (input === 7) {
+          return (numsToTen[4] + numsToTen[2])
+        }else if (input === 8) {
+          return (numsToTen[4] + numsToTen[3])
+        }else if (input === 9) {
+          return (numsToTen[1] + numsToTen[5])
+        }
+}
+
+
+}
 
 
 
@@ -28,9 +72,9 @@ return numsToTen[output]
 $(document).ready(function(){
   $("#form").submit(function(event){
   event.preventDefault();
-  var input = parseInt($("#inputNumber").val());
+  var input = $("#inputNumber").val();
 
-  var result = romanIndex(input);
+  var result = master(input);
   console.log(result)
 
   $("#result").val(result);
